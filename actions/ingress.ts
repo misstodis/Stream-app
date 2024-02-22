262badbb68afcfd1b4c1c1329a8b9cfefcb379c5
misstodis <nduc2312000@hotmail.com>
2024-02-22 20:29:04 +0100

    livekit to create ingress for connection to OBS

---
+++ actions/ingress.ts
@@ -0,0 +1,102 @@
+"use server";
+
+import { getSelf } from "@/lib/auth-service";
+import { db } from "@/lib/db";
+import {
+    type CreateIngressOptions,
+    IngressInput,
+    RoomServiceClient,
+    IngressVideoEncodingPreset,
+    TrackSource,
+    IngressVideoOptions,
+    IngressAudioEncodingPreset,
+    IngressAudioOptions,
+    IngressClient
+} from "livekit-server-sdk";
+import { revalidatePath } from "next/cache";
+
+const roomService = new RoomServiceClient(
+    process.env.LIVEKIT_API_URL!,
+    process.env.LIVEKIT_API_KEY!,
+    process.env.LIVEKIT_API_SECRET_KEY!,
+);
+
+const ingressClient = new IngressClient(
+    process.env.LIVEKIT_API_URL!,
+    process.env.LIVEKIT_API_KEY!,
+    process.env.LIVEKIT_API_SECRET_KEY!
+);
+
+export const resetIngress = async (hostIdentity: string) => {
+    const ingresses = await ingressClient.listIngress({
+        roomName: hostIdentity
+    });
+
+    const rooms = await roomService.listRooms([hostIdentity]);
+
+    for (const room of rooms) {
+        await roomService.deleteRoom(room.name);
+    }
+
+    for (const ingress of ingresses) {
+        if (ingress.ingressId) {
+            await ingressClient.deleteIngress(ingress.ingressId);
+        }
+    }
+}
+
+export const creatIngress = async (ingressType: IngressInput) => {
+    const currentUser = await getSelf();
+
+    //when creating a new ingress, we need to reset the old one
+    await resetIngress(currentUser.id);
+
+    // const options: CreateIngressOptions = {
+    //     name: currentUser.username,
+    //     roomName: currentUser.id,
+    //     participantName: currentUser.username,
+    //     participantIdentity: currentUser.id,
+    // };
+
+
+    // if (ingressType === IngressInput.WHIP_INPUT) {
+    //     options.bypassTranscoding = true;
+    // } else {
+    //     options.video = {
+    //         source: TrackSource.CAMERA,
+    //         encodingOptions: {
+    //             case: 'preset',
+    //             value: IngressVideoEncodingPreset.H264_1080P_30FPS_3_LAYERS,
+    //         }
+    //     } as IngressVideoOptions;
+
+    //     options.audio = {
+    //         source: TrackSource.MICROPHONE,
+    //         encodingOptions: {
+    //             case: 'preset',
+    //             value: IngressAudioEncodingPreset.OPUS_STEREO_96KBPS
+    //         }
+    //     } as IngressAudioOptions;
+    // };
+
+    // const ingress = await ingressClient.createIngress(ingressType, options);
+
+    // if (!ingress || !ingress.url || !ingress.streamKey) {
+    //     throw new Error('Failed to create ingress');
+    // }
+
+    // await db.stream.update({
+    //     where: {
+    //         userId: currentUser.id
+    //     },
+    //     data: {
+    //         ingressId: ingress.ingressId,
+    //         serverUrl: ingress.url,
+    //         streamKey: ingress.streamKey,
+    //     }
+    // });
+
+    // revalidatePath(`/u/${currentUser.username}/keys}`);
+
+    // return ingress;
+}
--- app/(dashboard)/u/[username]/(home)/keys/_component/connect-modal.tsx
+++ app/(dashboard)/u/[username]/(home)/keys/_component/connect-modal.tsx
@@ -1,13 +1,44 @@
+'use client'
+
+import { creatIngress } from '@/actions/ingress'
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
 import { Button } from '@/components/ui/button'
 import { Dialog, DialogClose, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
+import { IngressInput } from 'livekit-server-sdk'
 import { Drumstick } from 'lucide-react'
-import React from 'react'
+import React, { ElementRef, useRef, useState, useTransition } from 'react'
+import { toast } from 'sonner'
 
 type Props = {}
 
+const RTMP = String(IngressInput.RTMP_INPUT);
+const WHIP = String(IngressInput.WHIP_INPUT);
+
+type IngressType = typeof RTMP | typeof WHIP;
+
 export default function ConnectModal({ }: Props) {
+
+    //this is a reference to the close button, this will close the modal after the connection is created
+    const closeRef = useRef<ElementRef<"button">>(null);
+
+    const [ingressType, setIngressType] = useState<IngressType>(RTMP);
+    const [isPending, startTransition] = useTransition();
+
+    const onSubmitCreateIngress = () => {
+        closeRef.current?.click();
+        startTransition(() => {
+            creatIngress(parseInt(ingressType))
+                .then(() => {
+                    toast.success('Connection created');
+                    closeRef.current?.click();
+                })
+                .catch((error) => {
+                    toast.error(error);
+                })
+        });
+    }
+
     return (
         <Dialog>
             <DialogTrigger asChild>
@@ -22,13 +53,17 @@ export default function ConnectModal({ }: Props) {
                         Genrerate connection
                     </DialogTitle>
                 </DialogHeader>
-                <Select>
+                <Select
+                    disabled={isPending}
+                    value={ingressType}
+                    onValueChange={(value) => setIngressType(value)}
+                >
                     <SelectTrigger className='w-full'>
                         <SelectValue placeholder='Ingress Type' />
                     </SelectTrigger>
                     <SelectContent>
-                        <SelectItem value='RTMP'>RTMP</SelectItem>
-                        <SelectItem value='WHIP'>WHIP</SelectItem>
+                        <SelectItem value={RTMP}>RTMP</SelectItem>
+                        <SelectItem value={WHIP}>WHIP</SelectItem>
                     </SelectContent>
                 </Select>
                 <Alert>
@@ -41,12 +76,19 @@ export default function ConnectModal({ }: Props) {
                     </AlertDescription>
                 </Alert>
                 <div className='flex justify-between'>
-                    <DialogClose>
+                    <DialogClose
+                        asChild
+                        ref={closeRef}
+                    >
                         <Button variant={'ghost'}>
                             Cancel
                         </Button>
                     </DialogClose>
-                    <Button variant={'primary'}>
+                    <Button
+                        disabled={isPending}
+                        variant={'primary'}
+                        onClick={onSubmitCreateIngress}
+                    >
                         Generate
                     </Button>
                 </div>
